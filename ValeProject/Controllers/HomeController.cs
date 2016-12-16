1c224c15255706ac7f3ceec842963f68b56b1e89
kaantas <kaantas@anadolu.edu.tr>
2016-12-16 13:53:09 +0300

    first commit

---
+++ ValeProject/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ValeDBEntities db = new ValeDBEntities();
+        // GET: Home
+        //MainPage
+        public ActionResult Index()
+        {
+            List <Musteri> ls = db.Musteri.ToList();
+            return View(ls);
+        }
+        //Üye Girişi
+        public ActionResult UyeGirisi()
+        {
+            return View();
+        }
+        //Üye Ol
+        public ActionResult UyeOl()
+        {
+            return View();
+        }
+        //Yönetici Girişi
+        public ActionResult YoneticiGirisi()
+        {
+            return View();
+        }
+        //İletişim
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ ValeProject/Scripts/HeaderActive.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    var getHref = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+    var Href = "/Home/" + getHref;
+    $(".nav>li>a")
+        .each(function () {
+            if ($(this).attr("href") == Href || $(this).attr("href") == '') {
+                var id = ".nav>#" + getHref;
+                $(id).addClass("active");
+            }
+        });
+});
