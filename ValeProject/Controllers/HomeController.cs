7f224d538ee5952b95af094b5724ad7fb6f7c44a
kaantas <kaantas@anadolu.edu.tr>
2016-12-19 11:53:50 +0300

    sefer sorgulama bilet al sayfasında yapıldı fakat tarih kontrolu yok

--- ValeProject/Controllers/HomeController.cs
+++ ValeProject/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.Core.Common.CommandTrees;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ValeProject.Models;
 
 namespace ValeProject.Controllers
 {
@@ -72,10 +74,57 @@ namespace ValeProject.Controllers
         {
             return View();
         }
-
         public ActionResult BiletAl()
         {
-            return View();
+            return View();   
+        }
+        [HttpPost]
+        public ActionResult BiletAl(FormCollection form)
+        {
+            Sefer model = new Sefer();
+            List<Sefer> ls=null;
+            string kalkisSehri = form["kalkisSehri"];
+            string varisSehri = form["varisSehri"];
+
+            /*
+            string tarih = form["tarih"]; // mm/dd/yyyy
+            string yil = tarih.Substring(6,4);
+            string ay = tarih.Substring(0, 2);
+            string gun = tarih.Substring(3, 2);
+            string sqlTarih = yil + "-" + ay + "-" + gun;
+            */
+            /*
+            string format;
+            DateTime r;
+            // Parse a date that includes seconds and milliseconds
+            // by using the French (France) and invariant cultures.
+            format = "yyyy-MM-dd";
+            CultureInfo provider = CultureInfo.CurrentCulture;
+            try
+            {
+                r = DateTime.ParseExact(tarih, format, provider);
+                Console.WriteLine("{0} converts to {1}.", tarih, r.ToString());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not in the correct format.", sqlTarih);
+            }
+            DateTime dt = Convert.ToDateTime(sqlTarih);
+          //  DateTime tarih = Convert.ToDateTime(form["tarih"]);
+             * */
+            var context = new ValeDBEntities();
+            var query = context.Sefer.ToList();
+            var result = query.Where(m => m.KalkisSehri == kalkisSehri && m.VarisSehri == varisSehri).ToList();
+            if (result.Count > 0)
+            {
+                ls = result;
+                return View(ls);
+            }
+            else
+            {
+                ViewBag.mesaj = "Sefer Bulunamadı !";
+                return View();
+            }
         }
     }
 }
