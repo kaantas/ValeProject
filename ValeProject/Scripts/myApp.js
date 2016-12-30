4298488d27add5515637ab85c9982dab8d1c03e5
kaantas <kaantas@anadolu.edu.tr>
2016-12-30 11:02:55 +0300

    otobus koltuk gorunteleme baslandı

--- ValeProject/Scripts/myApp.js
+++ ValeProject/Scripts/myApp.js
@@ -5,12 +5,12 @@ myApp.controller('biletController', function ($scope, $http) {
     $scope.v = false;
 
     $scope.go = function (seferId) {
-        console.log(seferId);
         $scope.v = true;
         $scope.biletler = [];
         $http.get("/Home/GetBilet/"+seferId)
             .then(function (result) {
                 $scope.biletler = result.data;
+                console.log($scope.biletler[0].KoltukNo);
             });
     }
 
